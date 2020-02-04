fbef17c42106790baef966acdac434cc832bc882
Danielle <ukka.ukka.nuka.nuka.appu.wa@gmail.com>
2020-02-03 18:25:45 -0800

    Added limited functionality to index buttons, added profile page.

---
+++ assets/script/index/script.js
@@ -0,0 +1,29 @@
+
+
+
+$("#profile").on("click", function(){
+    console.log("Profile button clicked!");
+    var newWindow = window.open("profile.html", "_blank");
+    if(newWindow){
+        newWindow.focus();
+
+
+    }
+
+    else{
+        alert("Opening new window failed");
+    }
+
+
+});
+
+
+$("#portfolio").on("click", function(){
+    var newWindow = window.open("portfolio.html", "_blank");
+    if(newWindow){
+        newWindow.focus();
+    }
+    else{
+        alert("Opening new window failed.");
+    }
+});
